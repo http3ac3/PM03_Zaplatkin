45aa84376d1f350cacc9072819828f6783401a4e
Yura Zaplatkin <stu-pksp120@AMDVLA3511GW12>
2024-04-19 11:39:15 +0300

    feat: изменение структруры проекта на верную

---
+++ PM03_Zaplatkin/src/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PM03_Zaplatkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = -1;
+            Console.Write("Введите количество поездок для создания (целое число больше 0): ");
+            while (!int.TryParse(Console.ReadLine(), out N) || N <= 0)
+            {
+                Console.Write("Ошибка! Число введено с ошибкой! Введите количество поездок для создания (целое число больше 0): ");
+            } 
+
+            TouristicOperator touristicOperator = new TouristicOperator(N);
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Поездка №" + (i + 1));
+                try
+                {
+                    Trip trip = new Trip();
+                    Console.Write("Введите длительность поездки (непустая строка): ");
+                    trip.SetDuration(Console.ReadLine());
+                    Console.Write("Введите цену поездки (число больше нуля): ");
+                    trip.SetPrice(decimal.Parse(Console.ReadLine())); ;
+                    Console.Write("Введите размер туристической группы поездки (число больше нуля): ");
+                    trip.SetTourGroupSize(int.Parse(Console.ReadLine()));
+                    touristicOperator.SetTripByIndex(trip, i);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Ошибка! Вы ввели отличное от числа значение! Заполните информацию о поездке заново!");
+                    i--;
+                    continue;
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Ошибка! Вы не ввели никакого значения! Заполните информацию о поездке заново!");
+                    i--;
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка! " + ex.Message + " Заполните информацию о поездке заново!");
+                    i--;
+                    continue;
+                }
+            }
+            Console.WriteLine("Несортированные данные: ");
+            foreach (var t in touristicOperator.GetTrips())
+            {
+                Console.WriteLine(t);
+            }
+
+            touristicOperator.SortByPriceAndTourGroupSizeAsc();
+
+            Console.WriteLine("\nОтсортированные данные по цене и размеру туристической группы: ");
+            foreach (var t in touristicOperator.GetTrips())
+            {
+                Console.WriteLine(t);
+            }
+
+            string catalogPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string fileName = @"tripsData.txt";
+            string fullFileName = Path.Combine(catalogPath, fileName);
+
+            touristicOperator.SaveTripsToFile(fullFileName);
+            Console.WriteLine("Отсортированные данные сохранены в файле по пути: '" + fullFileName + "'");
+        }
+    }
+}
---
+++ PM03_Zaplatkin/src/Trip.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM03_Zaplatkin
+{
+    class Trip
+    {
+        private string duration;
+        private decimal price;
+        private int tourGroupSize;
+
+        public string GetDuration()
+        {
+            return this.duration;
+        }
+
+        public void SetDuration(string duration)
+        {
+            if (duration.Length == 0)
+            {
+                throw new Exception("Информация о продолжительности поездки должна быть заполнена!");
+            }
+            this.duration = duration;
+        }
+
+        public decimal GetPrice()
+        {
+            return this.price;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            if (price <= 0)
+            {
+                throw new Exception("Цена поездки не может быть меньше или равна нулю!");
+            }
+            this.price = price;
+        }
+
+        public int GetTourGroupSize()
+        {
+            return this.tourGroupSize;
+        }
+
+        public void SetTourGroupSize(int tourGroupSize)
+        {
+            if (tourGroupSize <= 0)
+            {
+                throw new Exception("Размер туристической группы не может быть меньше или равен нулю!");
+            }
+            this.tourGroupSize = tourGroupSize;
+        }
+
+        public override string ToString()
+        {
+            return "Продолжительность поездки: " + this.duration + 
+                   ", Цена поездки: " + this.price +
+                   ", Размер туристической группы: " + this.tourGroupSize;
+        }
+    }
+}
